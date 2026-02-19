c14ba9d42b98551ae9d4d66a4c457c24b7b69dc4
nadiiakomar <komarnadia21@gmail.com>
2026-02-19 15:15:10 +0100

    Fixade stavfel, ändrade database-name i sql-kopplingen. Fungerar :)

--- GruppArbete/Program.cs
+++ GruppArbete/Program.cs
@@ -7,10 +7,12 @@ namespace TerminalBibliotek
     {
         static void Main(string[] args)
         {
-            using var connection = new SqlConnection("Server=localhost,1433;Database=Exempelnamn;User ID=sa;Password=Lösenord!;Encrypt=True;TrustServerCertificate=True;");
+            // Man behöver kolla så att alla ha databas som heter "Exempel" och Lösenord "Lösenord!" på dator (det ska vara MSSQL) Kolla ReadMe
+            using var connection = new SqlConnection("Server=localhost,1433;Database=Exempel; User ID=sa;Password=Lösenord!;Encrypt=True;TrustServerCertificate=True;");
             connection.Open();
 
-            connection.Execute("INSERT INTO ducks(name) VALUE('Kalle Anka')");
+            connection.Execute("INSERT INTO ducks(name) VALUES('Prov Anka')");
+            connection.Query<string>("SELECT name FROM ducks");
 
         }
     }
