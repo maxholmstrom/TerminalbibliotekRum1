61dc2655bf97d80982fdc26fc1ad2b3c7bc15c4d
nadiiakomar <komarnadia21@gmail.com>
2026-02-19 14:43:09 +0100

    Lade till SqlClient package Lade till Dapper package Lade till basic koppling till SQL

--- GruppArbete/Program.cs
+++ GruppArbete/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace TerminalBibliotek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var connection = new SqlConnection("Server=localhost,1433;Database=Exempelnamn;User ID=sa;Password=Lösenord!;Encrypt=True;TrustServerCertificate=True;");
+            connection.Open();
+
+            connection.Execute("INSERT INTO ducks(name) VALUE('Kalle Anka')");
+
+        }
+    }
+}
