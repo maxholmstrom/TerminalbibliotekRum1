4c6c26107c8123852bf8035a69b59cbbc5f1be71
nadiiakomar <komarnadia21@gmail.com>
2026-02-20 10:42:55 +0100

    Lade till hanteringen för böcker

--- GruppArbete/Program.cs
+++ GruppArbete/Program.cs
@@ -21,6 +21,19 @@ namespace TerminalBibliotek
                         );
                         END");
 
+
+            //SKAPA TABELLER - BOOK
+            connection.Execute(@"
+                    IF NOT EXISTS 
+                        (SELECT 1 FROM sys.tables WHERE name = 'Books')
+                           BEGIN
+                               CREATE TABLE Books (
+                                      Id INT IDENTITY(1,1) PRIMARY KEY,
+                                      Name NVARCHAR(100) NOT NULL UNIQUE,
+                                      
+                        );
+                        END");
+
             if (args.Length >= 3 &&
                 (args[0].Equals("add", StringComparison.OrdinalIgnoreCase) ||
                 args[0].Equals("a", StringComparison.OrdinalIgnoreCase)) &&
@@ -63,11 +76,63 @@ namespace TerminalBibliotek
                     Console.WriteLine($"- {author.Name} (ID: {author.Id})");
                 }
             }
+
+            if (args.Length >= 3 &&
+                (args[0].Equals("add", StringComparison.OrdinalIgnoreCase) ||
+                args[0].Equals("a", StringComparison.OrdinalIgnoreCase)) &&
+                (args[1].Equals("book", StringComparison.OrdinalIgnoreCase) ||
+                args[1].Equals("b", StringComparison.OrdinalIgnoreCase)))
+            {
+                string name = string.Join(" ", args.Skip(2)).Trim();
+
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    Console.WriteLine("Du måste ange ett namn.");
+                    return;
+                }
+
+                connection.Execute(
+                    "INSERT INTO Books (Name) VALUES (@Name)",
+                    new { Name = name });
+
+                Console.WriteLine("Book tillagd.");
+            }
+            else if (args.Length == 2 &&
+                    (args[0].Equals("list", StringComparison.OrdinalIgnoreCase) ||
+                    args[0].Equals("l", StringComparison.OrdinalIgnoreCase)) &&
+                    (args[1].Equals("books", StringComparison.OrdinalIgnoreCase) ||
+                    args[1].Equals("b", StringComparison.OrdinalIgnoreCase)))
+            {
+                var books = connection.Query<Book>("SELECT Id, Name FROM Authors").ToList();
+
+                if (books.Count == 0)
+                {
+                    Console.WriteLine("Inga böcker hittades.");
+                    return;
+                }
+
+                Console.WriteLine("Böcker:");
+                foreach (var book in books)
+                {
+                    Console.WriteLine($"- {book.Name} (ID: {book.Id})");
+                }
+            }
+
+
+
+
+
         }
         public class Author
         {
             public int Id { get; set; }
-            public string Name { get; set; }
+            public string Name { get; set; } = "";
+        }
+
+        public class Book
+        {
+            public int Id { get; set; }
+            public string Name { get; set; } = "";
         }
     }
 }
